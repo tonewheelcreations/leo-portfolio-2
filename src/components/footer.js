2e60b6ec58e827f5763358ca3c86b2ec7a3763cd
tonewheelcreations <68931996+tonewheelcreations@users.noreply.github.com>
2021-06-20 17:41:58 -0400

    fixed social icon margins in footer 02

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,6 @@
 
 import React from "react"
-import { footer, svg, svgContainer, footerContainer, tapTarget } from "./styles/footer.module.scss"
+import { footer, svg, svgContainer, footerContainer, tapTargetOne, tapTargetTwo } from "./styles/footer.module.scss"
 import linkedIn from "../svg/linkedin.svg"
 import behance from "../svg/behance.svg"
 
@@ -9,12 +9,12 @@ const Footer = () => (
     <div className={footerContainer}>
       <div className={svgContainer}>
         <a href="https://www.linkedin.com/in/leonid-sherman/" target="_blank" rel="noreferrer noopener">
-          <div className={tapTarget}>
+          <div className={tapTargetOne}>
             <img className={svg} src={linkedIn} alt="LinkedIn profile" />
           </div>
         </a>
         <a href="https://www.behance.net/leosherman1" target="_blank" rel="noreferrer noopener">
-          <div className={tapTarget}>
+          <div className={tapTargetTwo}>
             <img className={svg} src={behance} alt="Behance profile" />
           </div>
         </a>
